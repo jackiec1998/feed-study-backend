ad5e559617bc457b7c8bf2b6695219c689199f01
Jackie Chan <jackiec1998@gmail.com>
2024-08-15 15:41:41 -0500

    Simplest endpoint example to deploy to vercel.

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import time
+from fastapi import FastAPI
+import uvicorn
+import motor.motor_asyncio
+
+app = FastAPI()
+client = motor.motor_asyncio.AsyncIOMotorClient()
+database = client.events
+votes = database.get_collection('votes')
+
+@app.get('/')
+def hello_world():
+  return 'Hello, world!'
+
+@app.post('/upvote/')
+async def create_upvote():
+  _ = await votes.insert_one({
+    'type': 'up',
+    'created_utc': int(time.time())
+  })
+
+if __name__ == '__main__':
+  uvicorn.run(app)
+
