15ff76653ec822a7842581c00fb46224c41d19d7
Jackie Chan <jackiec1998@gmail.com>
2024-08-15 17:34:36 -0500

    Middleware handling CORS error.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import time
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import motor.motor_asyncio
 
@@ -8,6 +9,19 @@ app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ['uri'])
 database = client.events
 votes = database.get_collection('votes')
+
+origins = [
+    "http://localhost:5173",  # React app URL during development
+    "https://jackiec1998.github.io/",  # Replace with your frontend's production URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def hello_world():
