72d4bbb3903a403f206db06163d73d2ba72ac891
Jackie Chan <jackiec1998@gmail.com>
2024-08-19 18:35:23 -0500

    Added local time attribute to voting events. Seeing it Vercel won't yell at me anymore.

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import os
 import time
+import datetime as dt
+import pytz
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import motor.motor_asyncio
+
+def get_local_time():
+  utc_time = dt.datetime.now(pytz.utc)
+  central_time = utc_time.astimezone(pytz.timezone('America/Chicago'))
+  return central_time.strftime('%m/%d/%Y %I:%M %p')
 
 app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ['uri'])
@@ -36,7 +43,8 @@ async def create_upvote():
 
   vote = {
     'type': 'up',
-    'created_utc': int(time.time())
+    'created_utc': int(time.time()),
+    'local_time': get_local_time()
   }
 
   await votes.insert_one(vote)
