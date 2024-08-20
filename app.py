d4ed2ee4b8c374bbcc9173661199764980b64a91
Jackie Chan <jackiec1998@gmail.com>
2024-08-19 19:05:20 -0500

    Trying some Stackoverflow solution.

--- app.py
+++ app.py
@@ -25,6 +25,15 @@ origins = [
   # 'https://feed-study-backend.vercel.app'
 ]
 
+@app.middleware('http')
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
+
 app.add_middleware(
   CORSMiddleware,
   allow_origins=origins,
